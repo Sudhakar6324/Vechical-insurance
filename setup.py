dc7a79a0eb87ae9b2add885b236e76707def89d8
Sudhakar6324 <telugusudhakar42@gmail.com>
2025-08-06 11:58:09 +0530

    project sturtucre is ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Telugu Sudhakar",
+    author_email="telugusudhakar42@gmail.com",
+    packages=find_packages()
+)
